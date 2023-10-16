e065fe9204cc0363800200087b7141b4e5162742
Empitrix <empitrix@gmail.com>
2023-10-16 22:47:13 +0330

    Created: News Models

--- lib/models/news.dart
+++ lib/models/news.dart
@@ -3,23 +3,43 @@ import 'package:mosfet/backend/backend.dart';
 class News {
 	final String title;
 	final String topic;
+	final String source;
+	final String img;
+	final String date;
+	final List<String> texts;
+	final List<String> links;
 
 	News({
 		required this.title,
-		required this.topic
+		required this.topic,
+		required this.source,
+		required this.img,
+		required this.date,
+		required this.texts,
+		required this.links
 	});
 
 	toJson(){
 		return {
 			"title": title,
-			"topic": topic
+			"topic": topic,
+			"source": source,
+			"img": img,
+			"date": date,
+			"texts": texts,
+			"links": links
 		};
 	}
 
 	static toNews(Map input){
 		return News(
 			title: input["title"],
-			topic: input["topic"]
+			topic: input["topic"],
+			source: input["source"],
+			img: input["img"],
+			date: input["date"],
+			texts: input["texts"],
+			links: input["links"]
 		);
 	}
 
@@ -27,7 +47,10 @@ class News {
 	bool isEqual(News input){
 		if(
 			vStr(input.title) == vStr(title) &&
-			vStr(input.topic) == vStr(topic)
+			vStr(input.topic) == vStr(topic) &&
+			vStr(input.source) == vStr(source) &&
+			vStr(input.date) == vStr(date) &&
+			vStr(input.img) == vStr(img)
 		){
 			return true;
 		}
